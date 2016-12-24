bb397684c0c7e6a536f0210680322e9df68dfea4
Matthew Holman <mateoholman@gmail.com>
2016-12-24 14:20:13 -0600

    Added the User model

---
+++ models/UserModel.js
@@ -0,0 +1,25 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+const bcrypt = require('bcrypt');
+
+const userSchema = new Schema({
+  username: {
+    type: String,
+    unique: true,
+    lowercase: true,
+    required: true
+  },
+
+  password: {
+    type: String,
+    required: true
+  },
+
+  name: {
+    type: String,
+    required: false,
+    default: "Homer Sampson"
+  }
+});
+
+module.exports = mongoose.model('User', userSchema);
