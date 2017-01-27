550a1756be391fc05b8d9df5862f46aaf1a578a7
Matthew Holman <mateoholman@gmail.com>
2017-01-26 21:04:45 -0600

    Added showInfo functionality to BeerListItem

--- client/src/BeerListItem.js
+++ client/src/BeerListItem.js
@@ -10,14 +10,17 @@ class BeerListItem extends Component {
     super(props);
 
     this.state = {
-      id: props.beer._id,
-      label: props.beer.label,
-      name: props.beer.name
+      beer: props.beer,
+      showInfo: false
     }
   }
 
   handleDelClick(event) {
-    this.props.delBeer(this.state.id);
+    this.props.delBeer(this.state.beer.id);
+  }
+
+  handleInfoClick(event) {
+    this.state.showInfo ? this.setState({...this.state, showInfo: false}) : this.setState({...this.state, showInfo: true})
   }
 
   render() {
@@ -26,14 +29,24 @@ class BeerListItem extends Component {
       <div className="beer-container">
         <div className="beer-header">
           <div className="action-buttons">
-            <i className="fa fa-info" aria-hidden="true" ></i>
+            <i className="fa fa-info" aria-hidden="true" onClick={this.handleInfoClick.bind(this)}></i>
             <i className="fa fa-times" aria-hidden="true" onClick={this.handleDelClick.bind(this)}></i>
           </div>
-          <img src={this.state.label} alt="A cool beer label" />
-          <h2>{this.state.name}</h2>
+          <img src={this.state.beer.label} alt="A cool beer label" />
+          <h2>{this.state.beer.name}</h2>
         </div>
-        <div className="beer-info">
-        </div>
+        { this.state.showInfo ?
+          (<div className="beer-info">
+            <div id="abv-ibu-style">
+              <h4>ABV: {this.props.beer.abv}   |   IBU: {this.props.beer.ibu}</h4>
+              <h4>Style: {this.props.beer.style}</h4>
+            </div>
+            <div id="description">
+              <p>{this.props.beer.description}</p>
+            </div>
+          </div>)
+          : null
+        }
       </div>
     )
   }
